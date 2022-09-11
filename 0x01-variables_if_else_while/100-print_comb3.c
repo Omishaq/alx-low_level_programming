0a029fe67e9d463cbd2ab65fad3696694f22e599
Usman Mohammed <othmanmuhammad30@gmail.com>
2022-09-11 10:28:48 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,10 +20,7 @@ int main(void)
 
 		if (c != '8' || (c == '8' && i != '9'))																																					{
 		putchar(',');
-		putchar(' ');
-																																											}
-																																			}
-																	}
+		putchar(' ');																											}					}														}
 									}
 
 		putchar('\n');
