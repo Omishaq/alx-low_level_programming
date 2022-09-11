8e7055faf900b9bb593916da1fd88e62bcd11134
Usman Mohammed <othmanmuhammad30@gmail.com>
2022-09-11 09:14:44 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints some letters of alphabet
- *   
+ *
  * Return: Always (Success)
  */
 int main(void)
@@ -11,9 +11,9 @@ int main(void)
 
 		for (c = 'a'; c <= 'z'; c++)
 					{
-										if (c != 'e' && c != 'q')
-											{
-																	putchar(c);
+
+		if (c != 'e' && c != 'q')
+											{						putchar(c);
 																	}
 									}
 
