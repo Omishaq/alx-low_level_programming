9f96e278a52997aafdccfbcc3678fd57feedf49d
Usman Mohammed <othmanmuhammad30@gmail.com>
2022-09-13 12:42:54 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,6 +8,15 @@
  */
 int main(void);
 {
-	return (write(1, &c, 1));
+	putchar('_')
+		putchar('p')
+		putchar('u')
+		putchar('t')
+		putchar('c')
+		putchar('h')
+		putchar('a')
+		putchar('r')
+		putchar('\n')
+		return(0)
 
 }
