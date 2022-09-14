c7a192a527cb871bb50f5fa38d42a65e7caf7319
Usman Mohammed <othmanmuhammad30@gmail.com>
2022-09-14 09:19:43 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,10 @@
 
 /**
  * print_alphabet - Entry point
- *   
+ *
  * Description: program that prints the alphabets
  * followed by a new line
- *      
+ *
  * Return: Exit int function with an int 0
  */
 void print_alphabet(void)
