a7599f5c5dc5eaaddbe1b9823b23c5e5aacae448
Usman Mohammed <othmanmuhammad30@gmail.com>
2022-09-14 14:34:04 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,8 +9,8 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-									for (n = 'a'; n <= 'z'; n++)
-		_putchar(n);
-		_putchar('\n');
+	for (n = 'a'; n <= 'z'; n++)
+	_putchar(n);
+	_putchar('\n');
 	}
 }
