445878b907386b5c5e9c59e7e125f0162f9d7221
Usman Mohammed <othmanmuhammad30@gmail.com>
2022-09-15 13:34:28 -0700

    print num

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,8 @@ void print_most_numbers(void)
 
 	for (i = 48; i < 58; i++)
 	{
-	if (i == 50 || i == 52)
-		continue;
+		if (i == 50 || i == 52)
+			continue;
 		_putchar(i);
 	}
 	_putchar('\n');
