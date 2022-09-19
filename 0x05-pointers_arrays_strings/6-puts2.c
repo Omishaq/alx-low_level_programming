ed1918f7ec19e341f18329f159110f710faefb47
Usman Mohammed <othmanmuhammad30@gmail.com>
2022-09-19 10:54:24 -0700

    put 2 c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,7 +4,7 @@
 /**
  * puts2 - prints every character of a string
  * @str: A pointer to an int that will be changed
- *    
+ *
  * Return: void which means it is correct
  */
 void puts2(char *str)
